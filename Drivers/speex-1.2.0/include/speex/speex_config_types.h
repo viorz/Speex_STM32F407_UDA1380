5799a0f1699baf6c5ba9d9e51ced0f3c68a6fd9b
viorz <45625590+viorz@users.noreply.github.com>
2018-12-06 11:41:23 +0300

    Add files via upload

---
+++ Drivers/speex-1.2.0/include/speex/speex_config_types.h
@@ -0,0 +1,11 @@
+#ifndef __SPEEX_TYPES_H__
+#define __SPEEX_TYPES_H__
+
+
+typedef signed short spx_int16_t;
+typedef unsigned short spx_uint16_t;
+typedef signed int spx_int32_t;
+typedef unsigned int spx_uint32_t;
+
+#endif
+
